4082e49ea7ee73726e27bbd0a9557cf2df3d4305
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-08-05 16:26:28 -0300

    feat: adiciona filtro

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { Pencil, Plus, Search, Star } from 'lucide-react'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 import { getContacts } from '@/api/get-contacts'
 import { updateContactFavorite } from '@/api/update-contact-favorite'
@@ -14,11 +14,16 @@ import { queryClient } from '@/lib/react.query'
 export function Home() {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [idContact, setIdContact] = useState<string>('')
+  const [filterContact, setFilterContact] = useState('')
 
   const { data: result } = useQuery({
     queryKey: ['contacts'],
     queryFn: getContacts,
   })
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilterContact(e.target.value)
+  }
 
   const mutation = useMutation({
     mutationFn: updateContactFavorite,
@@ -51,7 +56,12 @@ export function Home() {
           <Header />
 
           <div className="mt-4 flex gap-4">
-            <Input startIcon={Search} placeholder="Procurar contato" />
+            <Input
+              startIcon={Search}
+              value={filterContact}
+              placeholder="Procurar contato"
+              onChange={handleInputChange}
+            />
             <DialogTrigger asChild>
               <Button onClick={handleOpenDialog}>
                 <Plus />
@@ -60,34 +70,40 @@ export function Home() {
           </div>
 
           {result &&
-            result.map((contact, index) => (
-              <div
-                key={contact.id}
-                className="mt-4 flex h-12 w-full items-center justify-between gap-2 rounded-sm px-2 hover:bg-slate-200"
-              >
-                {contact.nome}
-                <div className="flex gap-2">
-                  <DialogTrigger asChild key={index}>
+            result
+              .filter((contact) =>
+                contact.nome
+                  .toLowerCase()
+                  .includes(filterContact.toLowerCase()),
+              )
+              .map((contact, index) => (
+                <div
+                  key={contact.id}
+                  className="mt-4 flex h-12 w-full items-center justify-between gap-2 rounded-sm px-2 hover:bg-slate-200"
+                >
+                  {contact.nome}
+                  <div className="flex gap-2">
+                    <DialogTrigger asChild key={index}>
+                      <Button
+                        variant={'outline'}
+                        size={'icon'}
+                        onClick={() => updateContact(String(contact.id))}
+                      >
+                        <Pencil />
+                      </Button>
+                    </DialogTrigger>
                     <Button
                       variant={'outline'}
                       size={'icon'}
-                      onClick={() => updateContact(String(contact.id))}
+                      onClick={() =>
+                        handleFavoriteClick(contact.id, contact.favorito)
+                      }
                     >
-                      <Pencil />
+                      {contact.favorito ? <Star fill="#f59e0b" /> : <Star />}
                     </Button>
-                  </DialogTrigger>
-                  <Button
-                    variant={'outline'}
-                    size={'icon'}
-                    onClick={() =>
-                      handleFavoriteClick(contact.id, contact.favorito)
-                    }
-                  >
-                    {contact.favorito ? <Star fill="#f59e0b" /> : <Star />}
-                  </Button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
         </div>
       </div>
       <CreateNewContact
