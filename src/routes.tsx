b92cd25277b43065106a4384a12e6e2e60d6c450
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-08-05 16:17:37 -0300

    feat: ajusta o arquivo de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { Detail } from './pages/details'
 import { Home } from './pages/home'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  },
-  {
-    path: 'detail/:contactId',
-    element: <Detail />,
   },
 ])
